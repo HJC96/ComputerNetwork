05f075b0781f41d2bd86bf65e0d776db58c863f9
HJC96 <gkswlcjs2@naver.com>
2023-02-28 23:24:09 +0900

    Socket porting

--- SMTP/Mysmtp.cpp
+++ SMTP/Mysmtp.cpp
@@ -2,12 +2,33 @@
 #include <string>
 #include <vector>
 #include <sys/socket.h>
+#include <netinet/in.h> // for using sockaddr_in structure
 
+
+
+struct sockaddr {
+	__uint8_t       sa_len;         /* total length */
+	sa_family_t     sa_family;      /* [XSI] address family */
+#if __has_ptrcheck
+	char            sa_data[__counted_by(sa_len - 2)];
+#else
+	char            sa_data[14];    /* [XSI] addr value (actually smaller or larger) */
+#endif
+};
 
 
 int main()
 {
+    int client_socket;
+    struct sockaddr sock_addr;
 
+    memset(&sock_addr, 0, sizeof(sock_addr));
+    sock_addr.sa_family = AF_INET; // UDP, TCP, ETC
+    sock_addr.sa_data[0] = htonl // port, ip ? or ip, port?
     
+    
+    socklen_t address_len;
+    client_socket = socket(AF_INET, SOCK_STREAM, 0); // domain, type, protocol
+    connect(client_socket, sock_addr.sa_data, address_len);
 }
 
