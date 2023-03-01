39c3a56d3935898f64dcceaa53170351c5d45aa9
HJC96 <gkswlcjs2@naver.com>
2023-03-01 13:58:12 +0900

    hard coding

--- SMTP/Mysmtp.cpp
+++ SMTP/Mysmtp.cpp
@@ -24,8 +24,9 @@ int main()
     //*(char*)&mh.msg_name = *msg;
     memset(&sock_addr, 0, sizeof(sock_addr));
     sock_addr.sa_family = AF_INET;                                          // UDP, TCP, ETC
-    *(unsigned short*)&sock_addr.sa_data[0] = htons(587);                    // htons(),htonl():'host'to'network'short/long \ port: 2byte, ip:4byte, else 0 
+    *(unsigned short*)&sock_addr.sa_data[0] = htons(587);                    // htons(),htonl():'host'to'network'short/long \ port: 2byte, ip:4byte, else 0 , 465: 587:TLS 
     *(unsigned long*)&sock_addr.sa_data[2] = htonl(1823571308);             //  "108.177.125.108" smtp.gmail.com
+    
     
     client_socket = socket(AF_INET, SOCK_STREAM, 0); // domain, type, protocol
     if(connect(client_socket, &sock_addr, sizeof(sock_addr)) == -1)
@@ -34,18 +35,37 @@ int main()
     
     recv(client_socket, buffer, 256, 0);
     printf("%s", buffer);
-    ret = send(client_socket, "HELO smtp.google.com\r\n",sizeof("HELO smtp.google.com\r\n"), 0);
+    ret = send(client_socket, "EHLO smtp.google.com\r\n",sizeof("HELO smtp.google.com\r\n"), 0);
     printf("Send %d bytes\n",ret);
 
+    recv(client_socket, buffer, 512, 0);
+    printf("%s", buffer);
+
+    ret = send(client_socket, "STARTTLS\r\n",sizeof("STARTTLS\r\n"), 0);
+    printf("Send %d bytes\n",ret);
     recv(client_socket, buffer, 256, 0);
     printf("%s", buffer);
 
-    ret = send(client_socket, "MAIL FROM:<gkswlcjs2@naver.com>\r\n",sizeof("MAIL FROM:<gkswlcjs2@naver.com>\r\n"), 0);
-    printf("Send %d bytes\n",ret);
-    //sleep(1);
-    ret = send(client_socket, "RCPT TO:<jchanvvv@gmail.com>\r\n",sizeof("RCPT TO:<jchanvvv@gmail.com>\r\n"), 0);
-    recv(client_socket, buffer, 256, 0);
-    printf("%s", buffer);
+    // ret = send(client_socket, "AUTH LOGIN\r\n",sizeof("AUTH LOGIN\r\n"), 0);
+    // recv(client_socket, buffer, 512, 0);
+    // printf("%s", buffer);
+
+    // ret = send(client_socket, "mail from:<gkswlcjs2@naver.com>\r\n",sizeof("mail from:<gkswlcjs2@naver.com>\r\n"), 0);
+    // printf("Send %d bytes\n",ret);
+
+    // recv(client_socket, buffer, 256, 0);
+    // printf("%s", buffer);
+
+
+    // //sleep(1);
+    // ret = send(client_socket, "RCPT TO:<jchanvvv@gmail.com>\r\n",sizeof("RCPT TO:<jchanvvv@gmail.com>\r\n"), 0);
+    // recv(client_socket, buffer, 256, 0);
+    // printf("%s", buffer);
+
+    // ret = send(client_socket, "DATA\r\n",sizeof("DATA\r\n"), 0);
+    // recv(client_socket, buffer, 256, 0);
+    // printf("%s", buffer);
+
 
 }
 
