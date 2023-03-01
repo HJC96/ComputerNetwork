691832834075facf4db740468fe2c50cbba59a72
HJC96 <gkswlcjs2@naver.com>
2023-03-01 11:23:37 +0900

    hard cording

--- SMTP/Mysmtp.cpp
+++ SMTP/Mysmtp.cpp
@@ -4,20 +4,6 @@
 #define uint4_t uint16_t
 #define uint6_t uint16_t
 
-
-// typedef struct
-// {
-//     uint16_t src_port;
-//     uint16_t dst_prot;
-//     uint32_t ACK;
-//     uint4_t  HLEN;
-//     uint4_t  RSV;
-//     uint6_t  CON;
-//     uint16_t WIN;
-//     uint16_t CheckSum;
-//     uint16_t URGPtr;
-// }TCP_layout;
-
 const char* connect_err = "Connect Error";
 void error_msg(const char* msg);
 
@@ -25,21 +11,14 @@ int main()
 {
     int client_socket;
     struct sockaddr sock_addr;
-    const char* msg = "MAIL FROM:<gkswlcjs2@naver.com>";
+    //const char* msg = "MAIL FROM:<gkswlcjs2@naver.com>\r\n"; // According to the SMTP spec, added <CR><LF> 
+    //const char* msg2 = "RCPT TO:<jchanvvv@gmail.com>\r\n";
+
     struct msghdr mh;
 	char buffer[2048];
 
+    int ret = 0;
 
-    //TCP_layout msg;
-    // msg.src_port = 0b11111111;
-    // msg.dst_prot = 0b11111111;
-    // msg.ACK      = 0b1111;
-    // msg.HLEN     = 0b1111;
-    // msg.RSV      = 0b1111;
-    // msg.CON      = 0b1111;
-    // msg.WIN      = 0b1111;
-    // msg.CheckSum = 0b11111111;
-    // msg.URGPtr   = 0b11111111;
     memset(&mh, 0, sizeof(mh));
 
     //*(char*)&mh.msg_name = *msg;
@@ -53,9 +32,21 @@ int main()
         error_msg(connect_err);
 
     
-    send(client_socket, &msg,sizeof(msg), 0);
     recv(client_socket, buffer, 256, 0);
     printf("%s", buffer);
+    ret = send(client_socket, "HELO smtp.google.com\r\n",sizeof("HELO smtp.google.com\r\n"), 0);
+    printf("Send %d bytes\n",ret);
+
+    recv(client_socket, buffer, 256, 0);
+    printf("%s", buffer);
+
+    ret = send(client_socket, "MAIL FROM:<gkswlcjs2@naver.com>\r\n",sizeof("MAIL FROM:<gkswlcjs2@naver.com>\r\n"), 0);
+    printf("Send %d bytes\n",ret);
+    //sleep(1);
+    ret = send(client_socket, "RCPT TO:<jchanvvv@gmail.com>\r\n",sizeof("RCPT TO:<jchanvvv@gmail.com>\r\n"), 0);
+    recv(client_socket, buffer, 256, 0);
+    printf("%s", buffer);
+
 }
 
 
